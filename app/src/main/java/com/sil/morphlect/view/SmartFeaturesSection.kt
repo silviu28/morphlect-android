10f97af0bf99f0a070421cca26e051c4b01b9171
Silviu <183800984+silviu28@users.noreply.github.com>
2026-04-22 20:59:58 +0300

    (Possible fix for CI)

--- app/src/main/java/com/sil/morphlect/view/SmartFeaturesSection.kt
+++ app/src/main/java/com/sil/morphlect/view/SmartFeaturesSection.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.sil.morphlect.data.EvaluationResult
@@ -23,6 +24,7 @@ import com.sil.morphlect.view.dialog.impl.AddFunctionalityDialog
 import com.sil.morphlect.viewmodel.EditorViewModel
 import kotlin.random.Random
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SmartFeaturesSection(
     navController: NavController,
