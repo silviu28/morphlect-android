8b6f6a07ce53f023e93e103d47626bbd4824d3a7
Silviu <183800984+silviu28@users.noreply.github.com>
2026-04-22 20:45:14 +0300

    Fix ImageComparison

--- app/src/main/java/com/sil/morphlect/view/ImageComparison.kt
+++ app/src/main/java/com/sil/morphlect/view/ImageComparison.kt
@@ -1,6 +1,5 @@
 package com.sil.morphlect.view
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
@@ -8,11 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -23,13 +18,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.sil.morphlect.logic.FormatConverters
@@ -38,8 +31,11 @@ import com.sil.morphlect.viewmodel.EditorViewModel
 @Composable
 fun ImageComparison(vm: EditorViewModel, navController: NavController) {
     var dividerRatio by remember { mutableStateOf(.5) }
-    var original by remember { mutableStateOf(FormatConverters.matToBitmap(vm.originalMat!!)) }
-    var edited by remember { mutableStateOf(vm.previewBitmap!!) }
+    val original = FormatConverters.matToBitmap(vm.originalMat)
+    val edited = vm.layers
+        .reduce { allMerge, layer -> allMerge.mergeWith(layer) }
+        .visual
+        .asAndroidBitmap()
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -67,12 +63,13 @@ fun ImageComparison(vm: EditorViewModel, navController: NavController) {
                 Image(
                     bitmap = edited.asImageBitmap(),
                     contentDescription = null,
-                    contentScale = ContentScale.Crop
+                    contentScale = ContentScale.Fit
                 )
                 Image(
                     bitmap = original.asImageBitmap(),
                     contentDescription = null,
                     modifier = Modifier
+                        .fillMaxSize()
                         .drawWithContent {
                             clipRect(
                                 left = 0f,
@@ -83,7 +80,7 @@ fun ImageComparison(vm: EditorViewModel, navController: NavController) {
                                 this@drawWithContent.drawContent()
                             }
                         },
-                    contentScale = ContentScale.Crop
+                    contentScale = ContentScale.Fit
                 )
             }
 
